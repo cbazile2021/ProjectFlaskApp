7455742efe5f4dcafaab8f27acc2eea68461256b
cbazile2021 <102826069+cbazile2021@users.noreply.github.com>
2025-04-16 16:41:23 -0400

    updated server

--- main.py
+++ main.py
@@ -196,4 +196,5 @@ def get_uploaded_book(filename):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))
+    print(f"📦 Flask App Running on http://localhost:{port}")
     app.run(host='0.0.0.0', port=port, debug=True)
